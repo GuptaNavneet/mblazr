1a41d3c2311cbac2bb2cfc6d8cd9a4ff8f0ef8f7
ERic Nderitu <developer.ericke@gmail.com>
2020-12-14 14:15:42 +0300

    udated urls handlers

--- edgarapp/urls.py
+++ edgarapp/urls.py
@@ -5,8 +5,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
 from .views import (
-    HomePageView, SearchResultsView, SearchFilingView, AboutView, FaqView, contactView, 
-    login_view, register_view, logout_view, account_view, HedgeFundView, PlanView, PrinterView,filing_fetch
+    HomePageView, SearchResultsView, SearchFilingView, AboutView, FaqView, contactView,
+    login_view, register_view, logout_view, account_view, HedgeFundView, PlanView, PrinterView
 )
 
 urlpatterns = [
@@ -17,18 +17,16 @@ urlpatterns = [
     path('search/', SearchResultsView, name='companyOverview'),
     path('filing/', SearchFilingView, name='companyFiling'),
 
-    
     path('', HomePageView, name='home'),
 
     # member login side
     url(r'^memberhome/$', login_required(TemplateView.as_view(template_name='memberhome.html')), name='memberhome'),
-    url(r'^accounts/login/$', login_view, name = 'login'),
-    url(r'^accounts/register/$', register_view, name = 'register'),
-    url(r'^accounts/logout/$', logout_view, name = "logout"),
-    url(r'^account/$', account_view, name = "account"),
+    url(r'^accounts/login/$', login_view, name='login'),
+    url(r'^accounts/register/$', register_view, name='register'),
+    url(r'^accounts/logout/$', logout_view, name="logout"),
+    url(r'^account/$', account_view, name="account"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('pricing/', PlanView, name='pricing'),
     path('printer/<fid>/<start>/', PrinterView, name='printer'),
-    path('getfiling/<id>/<file>',filing_fetch,name='fetchfile')
 
 ]
